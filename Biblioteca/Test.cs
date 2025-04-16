86dcfb476142bad43f4b63e0c4a45909624eb180
Ana Galvez <anamariagalvez01@gmail.com>
2025-04-15 23:37:00 -0300

    feat: terminado parte de la biblioteca, para comenzar a hacer el trabajo

---
+++ Biblioteca/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblioteca = new Biblioteca();
+            cargarLibros(10);
+            cargarLibros(2);
+            biblioteca.listarLibros();
+            biblioteca.eliminarLibro("Libro 3");
+            Console.WriteLine("---------------------");
+            biblioteca.listarLibros();
+            cargarLibros(3);
+            Console.WriteLine("---------------------");
+            biblioteca.listarLibros();
+            void cargarLibros(int cantidad)
+            {
+                bool pudeCargar;
+                for (int i = 1; i <= cantidad ; i++)
+                {
+                    pudeCargar = biblioteca.agregarLibro("Libro " + i, "Autor " + i, "Editorial " + i);
+                    if (pudeCargar) Console.WriteLine("Libro " + i + " agregado correctamente.");
+                    else Console.WriteLine("Libro " + i + ". Ya existe en la biblioteca");
+
+                }
+            }
+        }
+    }
+}
