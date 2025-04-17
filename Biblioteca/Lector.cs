869b0943470acfbb991a5eec5e9ccfffea542999
Cardoso Virginia <virgicardoso@gmail.com>
2025-04-17 18:53:39 -0300

    Agrego clase Lector

---
+++ Biblioteca/Lector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    
+
+    class Lector
+    {
+        private string nombre;
+        private int dni;
+        private List<Libro> prestamos;
+
+        public Lector(string nombre, int dni)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+            this.prestamos = new List<Libro>();
+        }
+
+        public int getDni()
+        {
+            return dni;
+        }
+
+        private bool puedeRealizarPrestamo()
+        {
+            return prestamos.Count < 3;
+        }
+
+        public void agregarPrestamo(Libro libro)
+        {
+            if (puedeRealizarPrestamo())
+            {
+                prestamos.Add(libro);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Nombre: "+nombre+", DNI: "+dni+", Libros en préstamo: "+prestamos.Count;
+        }
+    }
+
+   
+
+    }
